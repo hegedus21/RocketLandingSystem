62bd61cd214c09ce9e295c6c8b8e7c07d60055f5
Péter Hegedüs <peter.hegedus@transcendh2o.com>
2022-06-28 20:51:52 +0300

    Added Position class to store position, points

---
+++ RocketLandingSystem/Position.cs
@@ -0,0 +1,24 @@
+﻿namespace RocketLandingSystem
+{
+    /// <summary>
+    /// Stores positions defined by the X and Y coordinates in the 2D space
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// X coordinate of the 2D space
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y coordinate of the 2D space
+        /// </summary>
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
