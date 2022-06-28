2a74eea4a5de98c08750e59897657d006e4991da
Péter Hegedüs <peter.hegedus@transcendh2o.com>
2022-06-28 21:10:40 +0300

    Added Area class to store square shape areas

---
+++ RocketLandingSystem/Area.cs
@@ -0,0 +1,30 @@
+﻿namespace RocketLandingSystem
+{
+    /// <summary>
+    /// Defines the area of a square shape.
+    /// </summary>
+    public class Area
+    {
+        /// <summary>
+        /// The top left point of the area
+        /// </summary>
+        public Position TopLeftCorner { get; set; }
+
+        /// <summary>
+        /// The width and length of the square
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="topLeftCorner">The position of the top, left corner</param>
+        /// <param name="length">The width and length of the area</param>
+        /// <exception cref="ArgumentNullException">The top, left corner must be defined</exception>
+        public Area(Position topLeftCorner, int length)
+        {
+            TopLeftCorner = topLeftCorner ?? throw new ArgumentNullException(nameof(topLeftCorner), "Invalid position: Position must not be null!");
+            Length = length;
+        }
+    }
+}
